ed79fe077be25ad408a915961f25498b3b0901cc
Luis Eduardo Jerez Girón <eduardo.devop@gmail.com>
2020-10-24 09:34:02 -0600

    Cuartiles arreglados

--- components/Quartiles.vue
+++ components/Quartiles.vue
@@ -3,20 +3,16 @@
     <table class="table">
         <tbody class="text-left">
           <tr>
-            <td class="bg-gray-100 whitespace-no-wrap"><b>Cuartil #1</b></td>
+            <td class="bg-gray-100 whitespace-no-wrap"><b>Cuartil #1 (Q<sub>1</sub>)</b></td>
             <td>{{$quartil1(data).toFixed(2)}}</td>
           </tr>
           <tr>
-            <td class="bg-gray-100 whitespace-no-wrap"><b>Cuartil #2</b></td>
+            <td class="bg-gray-100 whitespace-no-wrap"><b>Cuartil #2 (Q<sub>2</sub>)</b></td>
             <td>{{$quartil2(data).toFixed(2)}}</td>
           </tr>
           <tr>
-            <td class="bg-gray-100 whitespace-no-wrap"><b>Cuartil #3</b></td>
+            <td class="bg-gray-100 whitespace-no-wrap"><b>Cuartil #3 (Q<sub>3</sub>)</b></td>
             <td>{{$quartil3(data).toFixed(2)}}</td>
-          </tr>
-          <tr>
-            <td class="bg-gray-100 whitespace-no-wrap"><b>Cuartil #4</b></td>
-            <td>{{$quartil4(data).toFixed(2)}}</td>
           </tr>
         </tbody>
       </table>
