ca0c322272f31714afed896748bb0dcf46d8380b
Luis Eduardo Jerez Girón <eduardo.devop@gmail.com>
2020-10-24 11:13:50 -0600

    Espaciado entre tablas agregado y titulos cambiados

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
     <div class="grid grid-cols-1 md:grid-cols-2 gap-2 my-2">
       <div v-if="cleanData.length > 0" class="animate__animated animate__fadeIn">
 
-        <span class="inline-block text-lg mb-2">
+        <span class="inline-block text-xl font-bold mb-2">
           Datos limpios - {{ cleanData.length }} números encontrados
         </span>
         <textarea class="textarea" rows="6" disabled>{{ cleanData.join('\n') }}</textarea>
@@ -26,7 +26,7 @@
       </div>
       <div v-if="orderedData.length > 0" class="animate__animated animate__fadeIn">
 
-        <span class="inline-block text-lg mb-2">
+        <span class="inline-block text-xl font-bold mb-2">
           Datos ordenados - {{ orderedData.length }} números encontrados
         </span>
         <textarea class="textarea" rows="6" disabled>{{ orderedData.join('\n') }}</textarea>
@@ -34,38 +34,38 @@
       </div>
     </div>
 
-    <div v-if="cleanData.length > 0" class="my-2 animate__animated animate__fadeIn">
-      <span class="inline-block text-lg mb-2">
-        Tabla de datos
+    <div v-if="cleanData.length > 0" class="my-8 animate__animated animate__fadeIn">
+      <span class="inline-block text-xl font-bold mb-2">
+        Tabla de frecuencia
       </span>
       <DataTable :data="cleanData" :orderedData="orderedData" @onDataFormatted="(data) => dataFormatted = data" @onDataFormattedTotals="(data) => dataFormattedTotals = data"/>
     </div>
 
-    <div v-if="cleanData.length > 0" class="grid grid-cols-1 md:grid-cols-3 gap-3 my-2 animate__animated animate__fadeIn">
+    <div v-if="cleanData.length > 0" class="grid grid-cols-1 md:grid-cols-3 gap-3 my-8 animate__animated animate__fadeIn">
       <div>
-        <span class="inline-block text-lg mb-2">
+        <span class="inline-block text-xl font-bold mb-2">
           Cuartiles
         </span>
         <Quartiles :data="cleanData" class="mb-2"/>
 
-        <span class="inline-block text-lg mb-2">
+        <span class="inline-block text-xl font-bold mb-2">
           Deciles
         </span>
         <Decile :data="cleanData" class="mb-2"/>
 
-        <span class="inline-block text-lg mb-2">
+        <span class="inline-block text-xl font-bold mb-2">
           Percentiles
         </span>
         <Percentile :data="cleanData" class="mb-2"/>
       </div>
       <div>
-        <span class="inline-block text-lg mb-2">
+        <span class="inline-block text-xl font-bold mb-2">
           Medidas de tendencia central
         </span>
         <MeasuresOfCentralTendency :data="cleanData"/>
       </div>
       <div>
-        <span class="inline-block text-lg mb-2">
+        <span class="inline-block text-xl font-bold mb-2">
           Otros datos
         </span>
         <ExtraData :data="cleanData" :dataFormatted="dataFormatted" :dataFormattedTotals="dataFormattedTotals"/>
