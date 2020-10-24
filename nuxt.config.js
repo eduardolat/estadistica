6a6825bfd27fd99ccfbc8770927c96970487f927
Luis Eduardo Jerez Girón <eduardo.devop@gmail.com>
2020-10-24 08:06:33 -0600

    Modificaciones para que funcione en /estadistica/

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { hid: "description", name: "description", content: "" }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.svg" },
+      { rel: "icon", type: "image/x-icon", href: "./favicon.svg" },
       {
         rel: "stylesheet",
         href:
@@ -71,5 +71,10 @@ export default {
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {}
+  build: {},
+
+  // Configuración del router
+  router: {
+    base: "/estadistica/"
+  }
 };
