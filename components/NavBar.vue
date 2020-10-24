e7a47a43521bc0c62663e810f88dccd4e031168d
Luis Eduardo Jerez Girón <eduardo.devop@gmail.com>
2020-10-24 11:45:46 -0600

    Navbar fixed y blurred

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,41 +1,48 @@
 <template>
-  <div class="p-2 shadow animate__animated animate__fadeInDown animate__faster">
+  <div>
 
-    <div class="flex justify-between container mx-auto flex-wrap animate__animated animate__fadeInDown z-10">
+    <div class="animate__animated animate__fadeInDown animate__faster" id="fixed-nav-div">
 
-        <div class="flex">
+      <div class="flex justify-between container mx-auto flex-wrap animate__animated animate__fadeInDown z-10">
 
-          <nuxt-link to="/" class="flex">
+          <div class="flex">
 
-            <img src="icon.svg" class="w-18" alt="Ícono de la aplicación">
+            <nuxt-link to="/" class="flex">
 
-            <div class="flex flex-wrap content-center">
-              <span class="text-2xl ml-2">Estadística</span>
-            </div>
+              <img src="icon.svg" class="w-18" alt="Ícono de la aplicación">
 
-          </nuxt-link>
+              <div class="flex flex-wrap content-center">
+                <span class="text-2xl ml-2">Estadística</span>
+              </div>
 
-        </div>
+            </nuxt-link>
 
-        <div class="md:hidden text-3xl text-black hover:text-gray-700 z-10">
-          <i class="fas fa-bars cursor-pointer" v-if="!openMenu" @click="openMenu = !openMenu"></i>
-          <i class="fas fa-times cursor-pointer" v-if="openMenu" @click="openMenu = !openMenu"></i>
-        </div>
+          </div>
 
-        <div :class="{'hidden': !openMenu}" class="block w-full md:w-auto md:flex md:flex-wrap content-center animate__animated animate__fadeIn z-0">
+          <div class="md:hidden text-3xl text-black hover:text-gray-700 z-10">
+            <i class="fas fa-bars cursor-pointer" v-if="!openMenu" @click="openMenu = !openMenu"></i>
+            <i class="fas fa-times cursor-pointer" v-if="openMenu" @click="openMenu = !openMenu"></i>
+          </div>
 
-          <nuxt-link class="block text-center my-2 nav-link" to="/">Datos no agrupados</nuxt-link>
-          <!-- <nuxt-link class="block text-center my-2 nav-link" to="/grouped">Datos agrupados</nuxt-link>
-          <nuxt-link class="block text-center my-2 nav-link" to="/linealRegression">Regresión lineal</nuxt-link> -->
-          <a         class="block text-center my-2 github-link" href="https://github.com/eduardodevop/estadistica" target="_blank">
-            <i class="fab fa-github"></i> Github
-          </a>
+          <div :class="{'hidden': !openMenu}" class="block w-full md:w-auto md:flex md:flex-wrap content-center animate__animated animate__fadeIn z-0">
 
-        </div>
+            <nuxt-link class="block text-center my-2 nav-link" to="/">Datos no agrupados</nuxt-link>
+            <!-- <nuxt-link class="block text-center my-2 nav-link" to="/grouped">Datos agrupados</nuxt-link>
+            <nuxt-link class="block text-center my-2 nav-link" to="/linealRegression">Regresión lineal</nuxt-link> -->
+            <a         class="block text-center my-2 github-link" href="https://github.com/eduardodevop/estadistica" target="_blank">
+              <i class="fab fa-github"></i> Github
+            </a>
+
+          </div>
+
+      </div>
 
     </div>
 
+    <div class="w-full h-16"></div>
+
   </div>
+
 </template>
 
 <script>
@@ -49,6 +56,10 @@
 </script>
 
 <style scoped>
+
+#fixed-nav-div{
+  @apply p-2 shadow fixed z-40 w-full bg-white;
+}
 
 .nav-link {
   @apply mx-2 text-lg border-b-2 border-transparent;
@@ -78,6 +89,15 @@
   .nuxt-link-exact-active.nav-link{
     @apply border-purple-500;
   }
+
+  @supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
+    #fixed-nav-div {
+      -webkit-backdrop-filter: blur(10px);
+      backdrop-filter: blur(10px);
+      background-color: rgba(255, 255, 255, 0.8);
+    }
+  }
+
 }
 
 </style>
